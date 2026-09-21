55f9952126ca6b5d8df7d3754bf3ae6c0aa42f26
Okra <rafaelokra1002@gmail.com>
2026-09-21 00:15:18 -0300

    Update Facebook Pixel ID in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,10 +51,10 @@ export default function RootLayout({
       <body className={inter.className}>
         <Script id="fb-pixel" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `
 !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '411617598934749');
+fbq('init', '1074292432237603');
 fbq('track', 'PageView');
         ` }} />
-        <noscript><img height="1" width="1" style={{ display: "none" }} src="https://www.facebook.com/tr?id=411617598934749&ev=PageView&noscript=1" alt="" /></noscript>
+        <noscript><img height="1" width="1" style={{ display: "none" }} src="https://www.facebook.com/tr?id=1074292432237603&ev=PageView&noscript=1" alt="" /></noscript>
         <Providers>{children}</Providers>
         <PageViewTracker />
         <ServiceWorkerRegister />
